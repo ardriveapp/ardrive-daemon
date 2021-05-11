f3f319c0a2ae1cd3fb7f84dd6e9a24e0783c2496
Mati <matibat@gmail.com>
2021-05-10 21:24:34 -0300

    Updates added endpoints to client object

--- src/client/ClientService.ts
+++ src/client/ClientService.ts
@@ -1,16 +1,27 @@
 import { Socket } from 'net';
 import ipc from 'node-ipc';
-import { SERVICE_NAME, SERVICE_PATH } from '../constants';
-import { ADD_USER_ENDPOINT, ALL_ENDPOINTS, CREATE_WALLET_ENDPOINT } from '../endpoints';
+import { ErrorResponse, SERVICE_NAME, SERVICE_PATH } from '../constants';
+import {
+	ADD_DIVE_ENDPOINT,
+	ADD_USER_ENDPOINT,
+	ALL_ENDPOINTS,
+	AUTHENTICATE_USER,
+	CREATE_WALLET_ENDPOINT,
+	ECHO_ENDPOINT,
+	GET_UNSYNCED_DRIVES_ENDPOINT
+} from '../endpoints';
 import { Endpoint } from '../endpoints/Endpoint';
 import { getSocket } from '../constants';
 import { ArDriveUser, Wallet } from 'ardrive-core-js';
+import { DriveData } from '../constants';
+import { AuthenticationError } from '../errors';
 
 export class ClientService {
 	static instance: ClientService;
 	socket?: Socket;
 	private endpoints: Endpoint[] = ALL_ENDPOINTS;
 	private connectPromise?: Promise<void>;
+	private authenticated = false;
 
 	constructor() {
 		if (!ClientService.instance) {
@@ -30,56 +41,73 @@ export class ClientService {
 			return alreadyConnected;
 		}
 		if (!this.connectPromise) {
-			this.connectPromise = new Promise((resolve) => {
+			this.connectPromise = new Promise((resolve, reject) => {
 				ipc.connectTo(SERVICE_NAME, SERVICE_PATH, () => {
-					resolve();
 					this._setupHandlers();
+					this.socket?.once('connect', () => {
+						this.socket?.off('error', reject);
+						resolve();
+					});
+					this.socket?.once('error', reject);
 				});
 			});
 		}
 		return this.connectPromise;
 	};
 
-	disconnect = async (): Promise<void> => {
+	public disconnect = async (): Promise<void> => {
 		await ipc.disconnect(SERVICE_NAME);
 	};
 
-	isConnected(): boolean {
+	public isConnected(): boolean {
 		return !!this.socket;
 	}
 
-	_setupHandlers = (): void => {
+	private _setupHandlers = (): void => {
 		const socket = getSocket(SERVICE_NAME);
 		this.socket = socket;
 		if (!socket) {
-			console.error(`There's no socket!`);
-			return;
+			throw new Error("There's no socket!");
 		}
 		for (const endpoint of this.endpoints) {
 			socket.on(endpoint.name, endpoint.getClientHandler());
 		}
-		socket.on('socket.disconnected', function (_, destroyedSocketID) {
-			ipc.log('Server ' + destroyedSocketID + ' has disconnected!');
+		socket.on('error', (e: ErrorResponse | any) => {
+			if (e instanceof Error) {
+				console.error(e.message);
+			}
+		});
+		socket.on('socket.disconnected', function () {
+			ipc.log('Server has disconnected');
 		});
 	};
 
-	run = async <T>(endpointName: string, ...args: any): Promise<T> => {
+	// eslint-disable-next-line
+	public run = async <T>(endpointName: string, ...args: any): Promise<T> => {
 		const endpoint = this._findEndpoint(endpointName);
-		console.info(`Firing ${endpoint?.name}`);
+		if (endpoint?.requiresAuthentication && !this.authenticated) {
+			throw new AuthenticationError();
+		}
 		const tmp = endpoint?.fire(...args);
 		return (await tmp) as T;
 	};
 
-	_findEndpoint = (endpointName: string): Endpoint | undefined => {
+	private _findEndpoint = (endpointName: string): Endpoint | undefined => {
 		return this.endpoints.find((e: Endpoint) => e.name === endpointName);
 	};
 
-	createArDriveWallet = async (): Promise<Wallet> => {
+	public isOnline = async (): Promise<boolean> => {
+		const message = 'ping';
+		const echo = await this.run<string>(ECHO_ENDPOINT, message);
+		return message === echo;
+	};
+
+	public createArDriveWallet = async (): Promise<Wallet> => {
 		const wallet: Wallet = await this.run<Wallet>(CREATE_WALLET_ENDPOINT);
 		return wallet;
 	};
 
-	createArDriveUser = async (
+	public createArDriveUser = async (
 		login: string,
 		password: string,
 		wallet: Wallet,
@@ -95,5 +123,22 @@ export class ClientService {
 			autoSyncApproval: autoSyncApproval ? 1 : 0
 		};
 		return await this.run<string>(ADD_USER_ENDPOINT, user);
+	};
+
+	public authenticateArDriveUser = async (login: string, password: string): Promise<boolean> => {
+		const authenticated = await this.run<boolean>(AUTHENTICATE_USER, login, password);
+		if (authenticated) {
+			this.authenticated = true;
+		}
+		return authenticated;
+	};
+
+	public getUnsyncedDrives = async (login: string, drivePrivacy: 'private' | 'public'): Promise<DriveData[]> => {
+		return await this.run<DriveData[]>(GET_UNSYNCED_DRIVES_ENDPOINT, login, drivePrivacy);
+	};
+
+	public createArDrive = async (name: string, privacy: 'private' | 'public'): Promise<string> => {
+		// Return the new drive id
+		return await this.run<string>(ADD_DIVE_ENDPOINT);
 	};
 }
